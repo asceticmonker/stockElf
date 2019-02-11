e0dcc28aeb4f5f9e673ee02b7a602eb68ffe3ed1
asceticmonker <cy_ascetic@163.com>
2019-02-11 18:28:10 +0800

    update

--- controllers/uploadStock.js
+++ controllers/uploadStock.js
@@ -1,8 +1,24 @@
+const db = require('../model/db')
+let stockInfo = {
+  'stockName': '',
+  'stockNum': '',
+  'stockTime': '',
+  'stockfield': '',
+  'ZTreason': '',
+  'isZT': '',
+  'Zrange': ''
+}
 module.exports = {
   'POST /uploadStock': async (ctx, next) => {
     var email = ctx.request.body.email || '',
       password = ctx.request.body.password || '';
-    console.log(JSON.stringify(ctx.request.body))
+    for (let pattern in stockInfo) {
+      stockInfo[pattern] = ctx.request.body[pattern] || '';
+    }
+    db.insertOne('stockZt', stockInfo, function(err, info) {
+      console.log(err)
+      console.log(info)
+    })
 
   }
 };
